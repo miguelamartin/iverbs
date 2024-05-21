190e866622dacea216ecd6401e0c353f604c6fc1
Miguel Angel Martin Mendez <miguel.angel.martin@adevinta.com>
2024-05-21 21:40:37 +0200

    new list

--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,7 @@
 // data.js
 
 
-const verbs = [
+/*const verbs = [
   {infinitive: 'be', past: 'was/were', participle: 'been', spanish: 'ser, estar', remaining: 2},
   {infinitive: 'become', past: 'became', participle: 'become', spanish: 'convertirse, llegar a ser', remaining: 2},
   {infinitive: 'begin', past: 'began', participle: 'begun', spanish: 'empezar', remaining: 2},
@@ -23,6 +23,28 @@ const verbs = [
   {infinitive: 'drive', past: 'drove', participle: 'driven', spanish: 'conducir', remaining: 2},
   {infinitive: 'eat', past: 'ate', participle: 'eaten', spanish: 'comer', remaining: 2},
 ];
-
+*/
+const verbs = [
+  {infinitive: 'know', past: 'knew', participle: 'known', spanish: 'saber, conocer', remaining: 2},
+  {infinitive: 'learn', past: 'learnt', participle: 'learnt', spanish: 'aprender', remaining: 2},
+  {infinitive: 'leave', past: 'left', participle: 'left', spanish: 'salir, abandonar', remaining: 2},
+  {infinitive: 'lend', past: 'lent', participle: 'lent', spanish: 'prestar', remaining: 2},
+  {infinitive: 'let', past: 'let', participle: 'let', spanish: 'permitir', remaining: 2},
+  {infinitive: 'lose', past: 'lost', participle: 'lost', spanish: 'perder', remaining: 2},
+  {infinitive: 'make', past: 'made', participle: 'made', spanish: 'hacer, fabricar', remaining: 2},
+  {infinitive: 'meet', past: 'met', participle: 'met', spanish: 'reunirse, conocer', remaining: 2},
+  {infinitive: 'pay', past: 'paid', participle: 'paid', spanish: 'pagar', remaining: 2},
+  {infinitive: 'put', past: 'put', participle: 'put', spanish: 'poner', remaining: 2},
+  {infinitive: 'read', past: 'read', participle: 'read', spanish: 'leer', remaining: 2},
+  {infinitive: 'ring', past: 'rang', participle: 'rung', spanish: 'sonar', remaining: 2},
+  {infinitive: 'run', past: 'ran', participle: 'run', spanish: 'correr', remaining: 2},
+  {infinitive: 'say', past: 'said', participle: 'said', spanish: 'decir', remaining: 2},
+  {infinitive: 'see', past: 'saw', participle: 'seen', spanish: 'ver', remaining: 2},
+  {infinitive: 'sell', past: 'sold', participle: 'sold', spanish: 'vender', remaining: 2},
+  {infinitive: 'shine', past: 'shone', participle: 'shone', spanish: 'brillar', remaining: 2},
+  {infinitive: 'sing', past: 'sang', participle: 'sung', spanish: 'cantar', remaining: 2},
+  {infinitive: 'sit', past: 'sat', participle: 'sat', spanish: 'sentarse', remaining: 2},
+  {infinitive: 'sleep', past: 'slept', participle: 'slept', spanish: 'dormir', remaining: 2},
+]
 export default verbs;
 
